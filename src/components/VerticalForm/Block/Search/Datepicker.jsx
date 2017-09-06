b05b959ff5d4bdd6dd24941a4208b9a6277f5f4d
pnaumov <ecrofeg1@gmail.com>
2017-09-06 17:19:03 +0300

    Range of available dates

--- src/components/VerticalForm/Block/Search/Datepicker.jsx
+++ src/components/VerticalForm/Block/Search/Datepicker.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import NemoDatepicker from 'components/UI/Datepicker';
+import moment from 'moment';
 
 export default class Datepicker extends Component {
 	render() {
-		const { placeholder, type } = this.props;
+		const 
+			{ placeholder, type } = this.props,
+			minDate = moment(), // allow to pick dates between today...
+			maxDate = moment().add(1, 'years'); // ...and +1 year
 
 		return <div className={`col nemo-widget-form__${type}__date__col`}>
-			<NemoDatepicker excludeDates={[]} inputProps={{ placeholder }} />
+			<NemoDatepicker minDate={minDate} maxDate={maxDate} inputProps={{ placeholder }}/>
 		</div>;
 	}
 }
