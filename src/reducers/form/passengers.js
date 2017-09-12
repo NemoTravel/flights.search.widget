d72610aa3c3f2c2fda34cae00dd70a870dc07afc
pnaumov <ecrofeg1@gmail.com>
2017-09-13 00:47:08 +0300

    Get rid of `filterReducer`...

--- src/reducers/form/passengers.js
+++ src/reducers/form/passengers.js
@@ -1,28 +1,47 @@
 import { ADD_PASSENGER, REMOVE_PASSENGER } from 'actions';
-import { combineReducers } from 'redux';
-import { filterReducer } from 'reducers';
 
 const initialState = {
-	title: '',
-	code: '',
-	count: 0
+	ADT: {
+		title: 'Взрослые (12+)',
+		code: 'ADT',
+		count: 1
+	},
+	CLD: {
+		title: 'Дети (2-11)',
+		code: 'CLD',
+		count: 0
+	},
+	INF: {
+		title: 'Младенцы (0-2)',
+		code: 'INF',
+		count: 0
+	},
+	INS: {
+		title: 'Младенцы с местом (0-2)',
+		code: 'INS',
+		count: 0
+	}
 };
 
-function passengersReducer(state, { type, payload }) {
+function passenger(state, { type }) {
 	switch (type) {
 		case ADD_PASSENGER:
 			return { ...state, count: state.count + 1 };
-			
+
 		case REMOVE_PASSENGER:
 			return { ...state, count: state.count - 1 };
 	}
-	
+
 	return state;
 }
 
-export default combineReducers({
-	ADT: filterReducer('ADT', passengersReducer, { ...initialState, code: 'ADT', title: 'Взрослые (12+)', count: 1 }),
-	CLD: filterReducer('CLD', passengersReducer, { ...initialState, code: 'CLD', title: 'Дети (2-11)' }),
-	INF: filterReducer('INF', passengersReducer, { ...initialState, code: 'INF', title: 'Младенцы (0-2)' }),
-	INS: filterReducer('INS', passengersReducer, { ...initialState, code: 'INS', title: 'Младенцы с местом (0-2)' })
-});
+export default function passengersReducer(state = initialState, action) {
+	if (action.objectType) {
+		return {
+			...state,
+			[action.objectType]: passenger(state[action.objectType], action)
+		};
+	}
+
+	return state;
+};
