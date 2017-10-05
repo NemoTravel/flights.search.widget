a90aef4eb0f07ea3187641cd7ad49f6a99aaac2d
pnaumov <ecrofeg1@gmail.com>
2017-10-05 14:17:03 +0300

    React select plugin

--- src/components/Form/Search/Autocomplete.jsx
+++ src/components/Form/Search/Autocomplete.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Autosuggest from 'components/Form/Search/Autocomplete/Autosuggest/src';
 import classnames from 'classnames';
 import Tooltip from 'components/UI/Tooltip';
+import Select from 'react-select';
 
 export default class Autocomplete extends Component {
 	constructor(props) {
@@ -18,47 +19,20 @@ export default class Autocomplete extends Component {
 
 		this.fetchSuggestions = this.fetchSuggestions.bind(this);
 		this.onChangeHandler = this.onChangeHandler.bind(this);
-		this.onBlurHandler = this.onBlurHandler.bind(this);
-		this.onFocusHandler = this.onFocusHandler.bind(this);
-		this.onKeyDownHandler = this.onKeyDownHandler.bind(this);
-		this.clearSuggestions = this.clearSuggestions.bind(this);
-		this.clearAirport = this.clearAirport.bind(this);
-		this.renderSuggestion = this.renderSuggestion.bind(this);
+		this.renderOption = this.renderOption.bind(this);
 		this.renderInputField = this.renderInputField.bind(this);
-		this.selectSuggestion = this.selectSuggestion.bind(this);
-	}
-
-	/**
-	 * Clear loaded autocomplete suggestions.
-	 */
-	clearSuggestions() {
-		if (!this.props.system.routingGrid) {
-			this.props.changeAutocompleteSuggestions([], this.type);
-		}
-	}
-
-	/**
-	 * Reset selected airport.
-	 */
-	clearAirport() {
-		this.props.selectAirport(null, this.type);
-		this.props.changeAutocompleteInputValue('', this.type);
+		this.selectOption = this.selectOption.bind(this);
 	}
 
 	/**
 	 * Load autocomplete suggestions by given search string.
 	 * 
-	 * @param {String} value
-	 * @param {String} reason - why the method has been called
+	 * @param {String} searchText
 	 */
-	fetchSuggestions({ value, reason }) {
+	fetchSuggestions(searchText) {
 		const { sendAutocompleteRequest, system } = this.props;
-		const searchText = value;
 		
-		if (reason === 'input-focused') {
-			sendAutocompleteRequest('', this.type);
-		}
-		else if (searchText || system.routingGrid) {
+		if (searchText || system.routingGrid) {
 			// We don't want to harass servers too much.
 			clearTimeout(this.autocompleteTimeout);
 			
@@ -72,59 +46,29 @@ export default class Autocomplete extends Component {
 	/**
 	 * Change autocomplete value.
 	 * 
-	 * @param {Object} event
 	 * @param {String} searchString
 	 */
-	onChangeHandler(event, { newValue: searchString }) {
-		this.props.changeAutocompleteInputValue(searchString, this.type);
+	onChangeHandler(searchString) {
+		this.fetchSuggestions(searchString);
+		return searchString;
 	}
 
 	/**
-	 * Select all text in input when focused.
+	 * Render option element.
 	 * 
-	 * @param {Object} event
-	 */
-	onFocusHandler(event) {
-		if (event.target && event.target.value) {
-			event.target.setSelectionRange(0, event.target.value.length);
-		}
-		
-		this.setState({ isFocused: true });
-	}
-
-	/**
-	 * @param {Object} event
-	 */
-	onBlurHandler(event) {
-		this.setState({ isFocused: false });
-	}
-
-	/**
-	 * @param {Object} event
-	 */
-	onKeyDownHandler(event) {
-		if (event.keyCode === 13 && this.input) {
-			this.input.blur();
-			this.onBlurHandler(event);
-		}
-	}
-
-	/**
-	 * Render element from suggestions (autocomplete) list.
-	 * 
-	 * @param {Object} item
+	 * @param {Object} option
 	 * @returns {XML}
 	 */
-	renderSuggestion(item) {
-		const { airport, country, isDirect } = item;
+	renderOption(option) {
+		const { airport, country, isDirect } = option.value;
 		
 		return <div className="widget-form-airports__suggestion">
 			<span className={classnames('widget-form-airports__suggestion__title', { 'widget-form-airports__suggestion__title_bold': isDirect })}>
 				{airport.name}
 			</span>
-			
+
 			{country ? <span className="widget-form-airports__suggestion__countryName">{country.name}</span> : null}
-			
+
 			<span className="widget-form-airports__suggestion__code">
 				{airport.IATA}
 			</span>
@@ -137,14 +81,10 @@ export default class Autocomplete extends Component {
 	 * @returns {*}
 	 */
 	renderAirportCode() {
-		const { isLoading, airport } = this.props;
-		const isActive = !isLoading && airport;
-		let className = classnames(
-			'widget-form-airports__airportCode',
-			{ 'widget-form-airports__airportCode_withArrow': this.props.system.routingGrid }
-		);
-		
-		return isActive ? <span className={className}>{airport.IATA}</span> : null;
+		const { airport } = this.props;
+		return airport 
+			? <span className="widget-form-airports__airportCode widget-form-airports__airportCode_withArrow">{airport.IATA}</span> 
+			: null;
 	}
 
 	/**
@@ -154,18 +94,6 @@ export default class Autocomplete extends Component {
 	 */
 	renderSwitcher() {
 		return null;
-	}
-
-	/**
-	 * Render select-like arrow.
-	 * Enabled if `routingGrid` config parameter has been specified.
-	 * 
-	 * Sets focus on autocomplete input field and runs autocomplete request.
-	 * 
-	 * @returns {*}
-	 */
-	renderArrow() {
-		return this.props.system.routingGrid ? <div className="widget-ui-icon widget-ui-input__arrow"/> : null;
 	}
 
 	/**
@@ -194,71 +122,60 @@ export default class Autocomplete extends Component {
 	/**
 	 * Set selected airport to the state.
 	 * 
-	 * @param {Object} event
-	 * @param {Object} suggesting
+	 * @param {Object} option
 	 */
-	selectSuggestion(event, { suggestion }) {
-		this.props.selectAirport(suggestion.airport, this.type);
+	selectOption(option) {
+		this.props.selectAirport(option.value.airport, this.type);
 	}
 	
 	shouldComponentUpdate(nextProps, nextState) {
-		const { showErrors, suggestions, isLoading, inputValue, airport } = this.props;
+		const { showErrors, suggestions, isLoading, airport } = this.props;
 		
-		return this.state.isFocused !== nextState.isFocused || 
-			showErrors !== nextProps.showErrors ||
+		return showErrors !== nextProps.showErrors ||
 			suggestions !== nextProps.suggestions ||
-			inputValue !== nextProps.inputValue ||
 			airport !== nextProps.airport ||
 			isLoading !== nextProps.isLoading;
 	}
 	
 	render() {
-		const { suggestions, isLoading, inputValue, system:config } = this.props;
-		let inputClassName = classnames(
-			`form-control widget-form-airports__input`,
-			{ 'widget-form-airports__input_focused': this.state.isFocused },
-			{ 'widget-form-airports__input_loading': isLoading },
-			{ 'widget-form-airports__input_pointer': config.readOnlyAutocomplete && config.routingGrid }
-		);
+		const { suggestions, isLoading, system:config, airport } = this.props;
+		
+		const selectedValue = airport ? {
+			label: airport.name,
+			value: airport
+		} : null;
 		
 		return <div className="col widget-form-airports__col">
-			<div className={classnames('widget-form-airports__header', { 'widget-form-airports__header_visible': this.state.isFocused })}>
-				<div className="widget-ui-icon widget-ui-mobile__back" onClick={() => {
-				}}/>
-				{this.mobileTitle}
-				<div className="widget-form-airports__underlay"/>
-			</div>
+			{/*<div className={classnames('widget-form-airports__header', { 'widget-form-airports__header_visible': this.state.isFocused })}>*/}
+				{/*<div className="widget-ui-icon widget-ui-mobile__back" />*/}
+				{/*{this.mobileTitle}*/}
+				{/*<div className="widget-form-airports__underlay"/>*/}
+			{/*</div>*/}
 
-			<div className="widget-form-airports__input__wrapper">
-				<Autosuggest
-					ref={component => this.autosuggest = component}
-					id={this.type}
-					suggestions={suggestions}
-					onSuggestionsFetchRequested={this.fetchSuggestions}
-					onSuggestionsClearRequested={this.clearSuggestions}
-					getSuggestionValue={(item) => item.airport.name}
-					renderSuggestion={this.renderSuggestion}
-					onSuggestionSelected={this.selectSuggestion}
-					highlightFirstSuggestion={true}
-					focusInputOnSuggestionClick={false}
-					shouldRenderSuggestions={value => config.routingGrid || (value && value.length > 1)}
-					renderInputComponent={this.renderInputField}
+			<div className="widget-form-airports__select__wrapper">
+				<Select
+					clearable={false}
+					autoBlur={true}
+					autosize={false}
+					noResultsText={null}
+					openOnFocus={true}
+					className={classnames('widget-form-airports__select', { 'widget-form-airports__select_loading': isLoading })}
+					value={selectedValue}
+					options={suggestions}
+					onInputChange={this.onChangeHandler}
+					placeholder={this.placeholder}
+					onChange={this.selectOption}
+					onFocus={() => this.fetchSuggestions('')}
+					optionRenderer={this.renderOption}
+					arrowRenderer={() => <div className="widget-ui-icon widget-ui-input__arrow"/>}
 					inputProps={{
-						className: inputClassName,
 						spellCheck: false,
 						readOnly: config.readOnlyAutocomplete && config.routingGrid,
-						placeholder: this.placeholder,
-						value: inputValue,
-						onChange: this.onChangeHandler,
-						onFocus: this.onFocusHandler,
-						onBlur: this.onBlurHandler,
-						onKeyDown: this.onKeyDownHandler
 					}}
 				/>
 
 				{this.renderAirportCode()}
 				{this.renderSwitcher()}
-				{this.renderArrow()}
 			</div>
 		</div>;
 	}
