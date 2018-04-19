7df45dd0ee629f238ba21b46095bac6204b7852b
Anton-Malyugin <a.malyugin@mute-lab.com>
2018-04-19 17:14:47 +0400

    35702-m customization websky-theme for iraero

--- src/components/Form/Search/DatesContainer.tsx
+++ src/components/Form/Search/DatesContainer.tsx
@@ -56,7 +56,7 @@ class DatesContainer extends React.Component<StateProps & DispatchProps & Props>
 			returnInitialDate = returnDatepicker.date;
 		}
 
-		return <div className="form-group row widget-form-dates">
+		return <div className="widget-form-dates">
 			<DepartureDatepicker
 				showErrors={showErrors}
 				wrongDatesOrder={datesIsNotOrder}
